a3433e7fdc5c2139d6a3f1851940aaf8f32a3ab9
Daniel Bajj <daniel.bajj@gmail.com>
2019-11-24 21:54:32 -0800

    Minor fixes, generate jar

---
+++ src/main/java/cpsc304/util/LengthConstants.java
@@ -0,0 +1,8 @@
+package cpsc304.util;
+
+public class LengthConstants {
+    public static final int LICENSE_LENGTH = 7;
+    public static final int PHONE_LENGTH = 14;
+    public static final int EXPIRY_DATE_LENGTH = 5;
+    public static final int CREDIT_CARD_NUM_LENGTH = 16;
+}
